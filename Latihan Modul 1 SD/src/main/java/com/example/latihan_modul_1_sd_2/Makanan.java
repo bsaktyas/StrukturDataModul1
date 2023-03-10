bc1f3478e0395956898eae189279cdb88d901951
bsaktyas <127457319+bsaktyas@users.noreply.github.com>
2023-03-10 08:00:20 +0700

    Delete Latihan Modul 1 SD directory

--- Latihan Modul 1 SD/src/main/java/com/example/latihan_modul_1_sd_2/Makanan.java
+++
@@ -1,7 +0,0 @@
-package com.example.latihan_modul_1_sd_2;
-
-public class Makanan extends Hidangan{
-    public String disantap() {
-        return this.getNamaHidangan()+ " dimakan";
-    }
-}
--- Latihan Modul 1 SD/src/main/java/com/example/latihan_modul_1_sd_2/Minuman.java
+++
@@ -1,7 +0,0 @@
-package com.example.latihan_modul_1_sd_2;
-
-public class Minuman extends Hidangan{
-    public String disantap(){
-        return this.getNamaHidangan() + " diminum";
-    }
-}
